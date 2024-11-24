8bef990fdf8b9e32c2a9390de19d142cc0e10cc6
Matclown23 <mq.cancino@gmail.com>
2024-11-24 20:04:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("form-contacto").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("¡Gracias por tu mensaje! Nos pondremos en contacto pronto.");
+});
