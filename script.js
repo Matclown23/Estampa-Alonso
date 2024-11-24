a57e494b1f17ddda463fe9146897de23792ebde9
Matclown23 <mq.cancino@gmail.com>
2024-11-24 20:21:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("form-contacto").addEventListener("submit", function(e) 
     e.preventDefault();
     alert("¡Gracias por tu mensaje! Nos pondremos en contacto pronto.");
 });
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu-items');
+    menu.classList.toggle('show');
+  }
